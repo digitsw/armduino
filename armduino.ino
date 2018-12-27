b70dfdcf2db9a834c80fba1193fd065baa521fbe
Imad <baddin2016@gmail.com>
2018-12-27 19:41:37 +0100

    fix comments and add spaces between functions

--- armduino.ino
+++ armduino.ino
@@ -2,13 +2,14 @@
 #include "common.h"
 
 uint16_t potpin = 0;  /* Potentiometer input PIN 0*/
-uint16_t val[4]; /* Servo Arrays for Store  Analog Value */
-uint16_t map_val[4] ; /* Servo to store position  after Mapping Analog Value */
-uint16_t step=1; /* Array change steps to store it in array steps */
-uint16_t sero_pos[180]; /* Array for Store steps for one servo*/ 
-uint16_t step_max = 179; /* max steps */
+uint16_t val[4]; /* Servo array to store the analog values */
+uint16_t map_val[4] ; /* Servo array to store the position after mapping the analog value */
+uint16_t step=1; /* Array's "change steps" to store it in array steps */
+uint16_t sero_pos[180]; /* An array  Store the steps for a one servo*/ 
+uint16_t step_max = 179; /* Max steps */
 uint16_t ste=0;
 Servo servo_A,servo_B,servo_C,servo_D;
+
 void setup()
 {
     pinMode(RECORD_BUTT, INPUT);
@@ -20,9 +21,10 @@ void setup()
     servo_B.attach(SER_B_PIN); 
     servo_C.attach(SER_C_PIN);
     servo_D.attach(SER_D_PIN); 
-    init_servo(); /*set & write all servo angle to INIT_ANGLE*/
+    init_servo(); /* set & write all servo angles to INIT_ANGLE*/
     Serial.begin(9600);
 }
+
 void loop()
 {
     // readpot(); 
@@ -32,7 +34,7 @@ void loop()
             working();
             readpot();
             free_mode();
-            /* Check if Position changed sero_pos[step-1]!=map_val[0] */
+            /* Check if the position changed sero_pos[step-1]!=map_val[0] */
             if(sero_pos[step-1]!=map_val[0] && step<step_max )
                 {
                     Serial.println( map_val[0]);
@@ -55,15 +57,17 @@ void loop()
             else ste=0;
         }
 }
+
 /* working() 
- * function to turn on / turn of LED working
- *  when Servo is change angle */  
+ * a function to turn on/turn off the "LED working"
+ *  when Servo changes angle */  
 void working()
 {
     Serial.println("Working nee ");
     digitalWrite(LED_WORKING, HIGH);
     digitalWrite(LED_STANDBY, LOW);
 }
+
 void init_servo()
 {
     working();
@@ -74,17 +78,20 @@ void init_servo()
     delay(2000);
     standby();
 }
+
 void standby()
 {
     digitalWrite(LED_WORKING, LOW);
     digitalWrite(LED_STANDBY, HIGH);
    
 }
+
 void readpot()
 {
     val[0] = analogRead(potpin); 
     map_val[0]  = map(val[0], 0, 1023, 0, 180);     
 }
+
 void record()
 {
     Serial.print("Recording: ");
@@ -93,12 +100,14 @@ void record()
     Serial.println(step);
  
 }
+
 void play()
 {
     Serial.print("Playing: ");
     servo_A.write( sero_pos[ste]);
     Serial.print(sero_pos[ste]);
 }
+
 void free_mode()
 {
     servo_A.write(map_val[0]);
